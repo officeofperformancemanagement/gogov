dcb59ebcf660d83be87b6b9b891254b81fe3c95e
Jesse Springston <JSpringston@chattanooga.gov>
2025-06-10 13:58:19 -0400

    change version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     entry_points={
         "console_scripts": ["gogov=gogov.__init__:main"],
     },
-    version="0.7.0",
+    version="0.8.0",
     description="Unofficial API Client for GoGov CRM",
     long_description=long_description,
     long_description_content_type="text/markdown",
